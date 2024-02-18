16f904a404a424f1443a32c2d0d7d897130d57aa
Induwara <induwara2k@gmail.com>
2024-02-18 10:38:59 +0530

    get customer and save customer method added

--- Assesment-4/src/repository/CustomerRepository.java
+++ Assesment-4/src/repository/CustomerRepository.java
@@ -11,25 +11,51 @@ public class CustomerRepository {
 
 
     public CustomerRepository() {
-    session= SessionFactoryConfig.getInstance().getSession();
+        session = SessionFactoryConfig.getInstance().getSession();
     }
 
 
-    public int saveCustomer(Customer customer){
+    public int saveCustomer(Customer customer) {
         Transaction transaction = session.beginTransaction();
         try {
             int customerId = (int) session.save(customer);
             transaction.commit();
             return customerId;
-        }catch (Exception e){
+        } catch (Exception e) {
             transaction.rollback();
-            session.close();
             e.printStackTrace();
             return -1;
+        }finally {
+            session.close();
         }
     }
 
 
+    public Customer getCustomer(int id) {
+        try {
+            Customer customer = session.get(Customer.class, id);
+            return customer;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }finally {
+            session.close();
+        }
+    }
+
+
+    public boolean updateCustomer(Customer customer){
+        Transaction transaction = session.beginTransaction();
+        try{
+            session.update(customer);
+            transaction.commit();
+            return true;
+        }catch (Exception e){
+            return false;
+        }finally {
+            session.close();
+        }
+    }
 
 }
 
